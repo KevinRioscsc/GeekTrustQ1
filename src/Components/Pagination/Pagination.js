31016f6354faac191331dda90067ab7e8589cb0f
Kevin Rios <rioskevin89@gmail.com>
2023-02-27 19:25:03 -0800

    first commit

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -4,7 +4,7 @@ import { RxDoubleArrowRight } from "react-icons/rx";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { RiArrowLeftSLine } from "react-icons/ri";
 import "./style.css";
-
+//
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   const [currentPage, setPage] = useState(1);
   const pageNumbers = [];
